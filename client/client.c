60134ff4d4889c3e06c8ef04ab83b30c81acf1c7
mattmac12 <m.maclennan@hotmail.com>
2018-10-18 13:36:23 +0800

    Update [from lecture 8]

--- client/client.c
+++ client/client.c
@@ -9,8 +9,9 @@
 // Functionality
 
 // Close
+#define BUFSIZE (1024 * 256)
+#define SERV_TCP_PORT 40004
 
-const int BACK_LOG = 5;
 
 int createTCPSocket()
 {
@@ -73,8 +74,82 @@ void helpMenu()
 
 int main(int argc, char* argv[])
 {
-	int socket;
+	int socket, n, nr, nw, i = 0;
+	char buf1[BUFSIZE], buf2[BUFSIZE], host[BUFSIZE];
+	struct sockaddr_in ser_addr;
+	struct hostent *hp;
 
+	// Get server host name
+	if (argc == 1) // Assume server running on the local host
+	{
+		gethostname(host, sizeof(host));
+	}
+	else if (argc == 2) // Use the given host name
+	{
+		strcpy(host, argv[1]);
+	}
+	else
+	{
+		printf("Useage: %s [<server_host_name>]\n", argv[0]);
+		exit(1);
+	}
+
+	// Get host address and build a server socket address
+	bzero((char *)&ser_addr, sizeof(ser_addr));
+	ser_addr.sin_family = AF_INET;
+	ser_addr.sin_port = htons(SERV_TCP_PORT);
+
+	if ((hp = gethostbyname(host)) == NULL)
+	{
+		printf("Host %s not found.\n", host);
+		exit(1);
+	}
+
+	ser_addr.sin_addr.s_addr = *(u_long *) hp->h_addr;
+
+	// Creat TCP socket and connect socket to server address
 	socket = createTCPSocket();
+
+	if (connect(socket, (stuct sockaddr *) &ser_addr, sizeof(ser_addr)) < 0)
+	{
+		perror("Client connect");
+		exit(1);
+	}
+
+	while (++i)
+	{
+		printf("Client Input [%d]: ", i);
+		fgets(buf, BUFSIZE, stdin);
+		nr = strlen(buf);
+		if (buf[nr - 1] == '\n'
+		{
+			buf[nr - 1] = '\0';
+			--nr;
+		}
+
+		if (strcmp(buf, "quit" == 0))
+		{
+			printf("Bye from client\n");
+			exit(0);
+		}
+
+		if (nr > 0)
+		{
+			if ((nw = written(sd, buf, nr)) < nr)
+			{
+				printf("Client: send error\n");
+				exit(1);
+			}
+
+			if ((nr = readn(sd, buf, sizeof(buf))) <= 0)
+			{
+				printf("Client: receive error\n");
+				exit(1);
+			}
+
+			but[nr] = '\0';
+			printf("Server output[%d]: %s\n", i, buf);
+		}
+	}
 }
 

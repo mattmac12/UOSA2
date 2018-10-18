ede4f82b9f8618d170410b6032a58222606daeb5
AusJad <jarad53@hotmail.com>
2018-10-18 14:48:45 +0800

    added .h

--- server/server.c
+++ server/server.c
@@ -1,9 +1,4 @@
-#include  <string.h>     /* strlen(), strcmp() etc */
-#include  <errno.h>      /* extern int errno, EINTR, perror() */
-#include  <sys/types.h>  /* pid_t, u_long, u_short */
-#include  <sys/socket.h> /* struct sockaddr, socket(), etc */
-#include  <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
-/* and INADDR_ANY */
+#include "server.h"
 
 #define   SERV_TCP_PORT   40004           /* server port no */
 #define   BUFSIZE         (1024*256)
---
+++ server/server.h
@@ -0,0 +1,6 @@
+#include  <string.h>     /* strlen(), strcmp() etc */
+#include  <errno.h>      /* extern int errno, EINTR, perror() */
+#include  <sys/types.h>  /* pid_t, u_long, u_short */
+#include  <sys/socket.h> /* struct sockaddr, socket(), etc */
+#include  <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
+/* and INADDR_ANY */

847377bb9119ec959d0efd37d42323fe947d96e5
mattmac12 <m.maclennan@hotmail.com>
2018-11-01 18:49:09 +0800

    Add files via upload

--- server/stream.h
+++ server/stream.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
 
 #define BUFSIZE (1024 * 256)
 
-int readn(int fd, char *buf, int bufsize);
+//int readn(int fd, char *buf, int bufsize);
 
-int writen(int fd, char *buf, int nbytes);
+//int writen(int fd, char *buf, int nbytes);
 
 int sendCode(int socket, char* code); // 1 byte
 int sendFNLen(int socket, int len); // 2 bytes
